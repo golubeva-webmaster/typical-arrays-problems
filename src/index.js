6ad364e73d338df78e45be6942213f886bc9cad9
golubeva-webmaster <golubeva.webmaster@gmail.com>
2020-10-03 15:15:17 +0400

    feat: Avg completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if(arr === undefined || arr.length === 0) {return 0;}
+  else{
+    var min = arr[0];
+    arr.forEach(function(item){
+      if (item < min)
+        min = item;
+    });
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if(arr === undefined || arr.length === 0) {return 0;}
+  else{
+    var max = arr[0];
+    arr.forEach(function(item){
+      if (item > max)
+        max = item;
+    });
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  return (arr === undefined || arr.length === 0) ? 0 : arr.reduce((summ,item) => summ+item, 0) / arr.length;
 }
